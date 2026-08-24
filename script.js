ecf66eb02b77e0213ddd6993b8cb88097114fde2
Hakkenen <hkndourado@gmail.com>
2026-08-23 22:57:19 -0300

    Primeiro commit do carrinho

---
+++ script.js
@@ -0,0 +1,100 @@
+const nomeProduto = document.getElementById("nome-produto");
+const qtdProduto = document.getElementById("qtd-produto");
+const precoProduto = document.getElementById("preco-produto");
+const formulario = document.getElementById("nova-compra");
+const listaProdutos = document.getElementById("lista-produtos");
+const mostraTotal = document.getElementById("total-compra");
+
+let produtoEditando = null;
+let carrinho = [];
+let totalCompra = 0;
+
+function editarProduto(id){
+    const produto = carrinho.find(function(produto){
+        return produto.id === id ;
+    });
+
+    nomeProduto.value = produto.nome;
+    qtdProduto.value = produto.quantidade;
+    precoProduto.value = produto.preco;
+
+    produtoEditando = id;
+}
+
+function removerProduto(id) {
+    carrinho = carrinho.filter(function(produto){
+        return produto.id !== id;
+    });
+
+    atualizarCarrinho();
+}
+
+function atualizarCarrinho() {
+    listaProdutos.innerHTML = "";
+
+    totalCompra = 0;
+
+    carrinho.forEach(function(produto) {
+
+        listaProdutos.innerHTML += `
+        <div class="produto-card">
+
+            <div class="produto-info">
+                <h3>${produto.nome}</h3>
+                <p>${produto.quantidade} x R$ ${produto.preco.toFixed(2)}</p>
+            </div>
+        
+        <div class="produto-acoes">
+            <strong> R$ ${produto.subtotal.toFixed(2)}</strong>
+
+            <div>
+                <button class="botao-editar" onclick="editarProduto(${produto.id})">Editar</button>
+                <button class="botao-remover" onclick="removerProduto(${produto.id})">Remover</button>
+            </div>
+        </div>
+
+    </div>
+    `;
+
+    totalCompra += produto.subtotal;
+    });
+
+    mostraTotal.textContent = totalCompra.toFixed(2);
+}
+
+formulario.addEventListener("submit", function(evento){
+    evento.preventDefault();
+    
+    const qtd_Produto = Number(qtdProduto.value);
+    const preco_Produto = Number(precoProduto.value);
+    const subtotal = qtd_Produto * preco_Produto;
+
+    const produto = {
+        id: Date.now(),
+        nome: nomeProduto.value,
+        quantidade: qtd_Produto,
+        preco: preco_Produto,
+        subtotal: subtotal
+    };
+
+    if (produtoEditando === null) {
+        carrinho.push(produto);
+    } else {
+        // edição
+        const indice = carrinho.findIndex(function(produto){
+            return produto.id === produtoEditando;
+        });
+
+        produto.id = produtoEditando;
+
+        carrinho[indice] = produto;
+
+        produtoEditando = null;
+    }
+
+    atualizarCarrinho();
+
+    nomeProduto.value = "";
+    qtdProduto.value = "";
+    precoProduto.value = "";
+});
